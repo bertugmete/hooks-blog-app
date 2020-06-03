a5ed2910339f1de3a550fa3315860a12bbf928b9
bertugmete <bertumete95@gmail.com>
2020-06-03 14:26:51 +0300

    initial commiy

---
+++ src/pages/routes/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Switch, Route } from "react-router"
+
+import Article from "pages/article"
+import GlobalFeed from "pages/globalFeed"
+import Authentication from "pages/authentication"
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" component={GlobalFeed} exact />
+      <Route path="/login" component={Authentication} />
+      <Route path="/article" component={Article} />
+    </Switch>
+  )
+}
+export default Routes
